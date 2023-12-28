50cc3a442b804d3f77da3618d9b3fcaaedf87248
Gandobilis <lashadeveloper@gmail.com>
2023-12-28 22:35:45 +0400

    first commit

---
+++ src/main/java/ge/agrotech/apiutils/ApiUtilsApplication.java
@@ -0,0 +1,13 @@
+package ge.agrotech.apiutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUtilsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ge/agrotech/apiutils/ApiUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.agrotech.apiutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
