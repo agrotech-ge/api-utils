166c4b7e4133aa6656ad4ba0851c98673774cd69
Sandro Ghughunishvili <84451463+godfather-coder@users.noreply.github.com>
2024-01-18 19:09:52 +0400

    maven depandency

--- src/main/java/ge/agrotech/apiutils/ApiUtilsApplication.java
+++ src/main/java/ge/agrotech/apiutils/ApiUtilsApplication.java
@@ -1,24 +1,14 @@
 package ge.agrotech.apiutils;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Sort;
+
 
 
 @SpringBootApplication
 public class ApiUtilsApplication {
-
     public static void main(String[] args) {
-        //SpringApplication.run(ApiUtilsApplication.class, args);
-//        RequestObject<Test> object = new RequestObject<>();
-//        System.out.println(object.checkRequiredFields(RequestMethodType.GET));
-
-        Sort.Order springOrder = Sort.Order.asc("filed_to_sort");
-        Order order = new Order(springOrder);
-
-        System.out.println("Field: " + order.getField());
-        System.out.println("Direction: " + order.getDirection());
-
-
+        SpringApplication.run(ApiUtilsApplication.class, args);
 
     }
 
